6ed642bb12e9a94135d2a209d9d3f054faaa066f
Andre Casaes <andreb.casaes@gmail.com>
2024-08-23 20:03:27 -0300

    change ico

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import AdminPanel from './components/AdminPanel';
